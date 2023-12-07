b4021772e01c4f8942efc35b13355e67d561a7f3
Jacob Saucelo <jacobsaucelo32@gmail.com>
2023-12-07 12:11:46 +0800

    added conversion and readcsv

--- main.go
+++ main.go
@@ -1,7 +1,121 @@
 package main
 
-import "fmt"
+import (
+	"encoding/csv"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+type Cars struct {
+	cars []CarTypes
+}
+
+type CarTypes struct {
+	Manufacturer        string
+	Model               string
+	Sales_in_thousands  float64
+	__year_resale_value float64
+	Vehicle_type        string
+	Price_in_thousands  float64
+	Engine_size         float64
+	Horsepower          uint16
+	Wheelbase           float64
+	Width               float64
+	Length              float64
+	Curb_weight         float64
+	Fuel_capacity       float64
+	Fuel_efficiency     uint16
+	Latest_Launch       string
+	Power_perf_factor   float64
+}
 
 func main() {
-	fmt.Println("initial commit")
+
+	carSales, carSalesErr := os.Open("./assets/Car_sales.csv")
+	if carSalesErr != nil {
+		fmt.Println("ERROR: File error")
+		panic(carSalesErr)
+	}
+	defer carSales.Close()
+
+	fmt.Printf("SalesTYPE: %T", carSales)
+
+	ReadCsv(carSales)
+
 }
+
+func ReadCsv(cs *os.File) Cars {
+	csvReader := csv.NewReader(cs)
+	data, err := csvReader.ReadAll()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	c := Cars{}
+	for di, d := range data {
+		car := CarTypes{}
+		if len(d) < 16 {
+			fmt.Printf("Warning index %d is missing values.", di)
+			continue
+		}
+
+		car.Manufacturer = d[0]
+		car.Model = d[1]
+		car.Sales_in_thousands = StringToFloat(d[2])
+		car.__year_resale_value = StringToFloat(d[3])
+		car.Vehicle_type = d[4]
+		car.Price_in_thousands = StringToFloat(d[5])
+		car.Engine_size = StringToFloat(d[6])
+		car.Horsepower = StringToInt(d[7])
+		car.Wheelbase = StringToFloat(d[8])
+		car.Width = StringToFloat(d[9])
+		car.Length = StringToFloat(d[10])
+		car.Curb_weight = StringToFloat(d[11])
+		car.Fuel_capacity = StringToFloat(d[12])
+		car.Fuel_efficiency = StringToInt(d[13])
+		car.Latest_Launch = d[14]
+		car.Power_perf_factor = StringToFloat(d[15])
+
+		// fmt.Println("data: ", d, " length: ", len(d))
+	}
+
+	return c
+}
+
+func StringToFloat(s string) float64 {
+	f, err := strconv.ParseFloat(s, 64)
+	if err != nil {
+		fmt.Println("cant parse float", s)
+	}
+
+	return f
+}
+
+func StringToInt(s string) uint16 {
+	ui, err := strconv.ParseUint(s, 10, 16)
+	if err != nil {
+		fmt.Println("cant parse uint", s)
+	}
+
+	return uint16(ui)
+}
+
+/*
+	Manufacturer        string   Acura,
+	Model               string   Integra,
+	Sales_in_thousands  float64   16.919,
+	__year_resale_value float64   16.36,
+	Vehicle_type        string   Passenger,
+	Price_in_thousands  float64   21.5,
+	Engine_size         float64   1.8,
+	Horsepower          uint   140,
+	Wheelbase           float64   101.2,
+	Width               float64   67.3,
+	Length              float64   172.4,
+	Curb_weight         float64   2.639,
+	Fuel_capacity       float64   13.2,
+	Fuel_efficiency     uint   28,
+	Latest_Launch       string   2/2/2012,
+	Power_perf_factor   float64   58.28014952
+*/
